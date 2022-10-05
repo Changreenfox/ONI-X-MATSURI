ab1185800ddba42158154bbef176d3c1ffc1194b
rcn66 <rcorreanetto@ufl.edu>
2022-10-05 18:01:41 -0400

    Updated enemy scripts; created boss test scene

--- Scripts/States/AbstractStates/AIMotion.cs
+++ Scripts/States/AbstractStates/AIMotion.cs
@@ -4,20 +4,20 @@ using System;
 // Base class for walking & jumping of AI (Most functionality remains in Motion.cs)
 public abstract class AIMotion : Motion
 {
-    public override void Enter()
-    {
-        base.Enter();
-    }
+	public override void Enter()
+	{
+		base.Enter();
+	}
 
-    //Direction is altered in the states since it isn't user input
-    protected override void GetInputDirection()
-    {
-        return;
-    }
+	//Direction is altered in the states since it isn't user input
+	protected override void GetInputDirection()
+	{
+		return;
+	}
 
-    //Attacks are called in the states themselves, not user input
-    protected override void CheckAttack()
-    {
-        return;
-    }
+	//Attacks are called in the states themselves, not user input
+	protected override void CheckAttack()
+	{
+		return;
+	}
 }
