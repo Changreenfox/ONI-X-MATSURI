d3a090e63fb7a314ed0e5f020d30de1ab20c35b3
Chase Ruskin <c.ruskin@ufl.edu>
2022-11-03 22:30:13 -0400

    adds key support for start screen menu selection

--- Scripts/Scenes/StartScreen.cs
+++ Scripts/Scenes/StartScreen.cs
@@ -3,9 +3,8 @@ using System;
 
 public class StartScreen : SceneBase
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	// the selected button can either be 'start/play' or not 'start/play' ('quit')
+	private bool isStartSel = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -13,11 +12,32 @@ public class StartScreen : SceneBase
 		base._Ready();
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		// update the current selected on-screen button (play or quit)
+		bool toggleR = Input.IsActionPressed("ArrowRight");
+		bool toggleL = Input.IsActionPressed("ArrowLeft");
+		// move to the 'quit' button
+		if (toggleR == true && isStartSel == true) {
+			isStartSel = false;
+		// move to the 'play' button
+		} else if (toggleL == true && isStartSel == false) {
+			isStartSel = true;
+		}
+
+		// check if the user as confirmed their choice
+		bool isReqAct = Input.IsActionPressed("ActionA");
+		if (isReqAct == true) {
+			// play :)
+			if (isStartSel == true) {
+				GetTree().ChangeScene("res://Scenes/World.tscn");
+			// quit :(
+			} else {
+				GetTree().Quit();
+			}
+		}
+	}
 
 	public void _on_StartButton_pressed()
 	{
