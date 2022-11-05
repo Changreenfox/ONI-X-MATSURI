09a1f03879950daa299371c3b33dc7216d88aca2
Elizabeth <echurinov@ufl.edu>
2022-11-05 15:09:57 -0400

    Adding in Drum Bounce

---
+++ Scenes/Prefabs/Tilemaps/Tower/DrumBounce.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class DrumBounce : Area2D
+{
+	protected GameManager gManager;
+	protected Player player;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		gManager = GetNode<GameManager>("/root/GameManager");
+		player = gManager.PlayerRef;
+	}
+
+	private void _on_Area2D_body_entered(object body)
+	{
+		if(player.GlobalPosition[1] < GlobalPosition[1]){//if the body is entered from on-top
+			if(player.Velocity[1] > 0){
+				Vector2 newVelocity = player.Velocity;
+				newVelocity[1] = -1500;
+				player.Velocity = newVelocity;
+			}
+		}
+	}
+
+}
