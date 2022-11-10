102491c8d1770c7dda5c58be67dbafcaab0ff658
Elizabeth <echurinov@gmail.com>
2022-11-10 00:07:26 -0500

    Adding Animation to Drums

--- Scenes/Prefabs/Tilemaps/Tower/DrumBounce.cs
+++ Scenes/Prefabs/Tilemaps/Tower/DrumBounce.cs
@@ -5,11 +5,13 @@ public class DrumBounce : Area2D
 {
 	protected GameManager gManager;
 	protected Player player;
+	protected AnimationPlayer animation;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		gManager = GetNode<GameManager>("/root/GameManager");
 		player = gManager.PlayerRef;
+		animation = GetParent().GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	private void _on_Area2D_body_entered(object body)
@@ -19,6 +21,7 @@ public class DrumBounce : Area2D
 				Vector2 newVelocity = player.Velocity;
 				newVelocity[1] = -1500;
 				player.Velocity = newVelocity;
+				animation.Play("Bounce");
 			}
 		}
 	}
