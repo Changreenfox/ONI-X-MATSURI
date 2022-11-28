3e1560d50b37dcd1a02330080ac7e69554fb9437
rcn66 <rcorreanetto@ufl.edu>
2022-11-28 04:50:56 -0500

    Created decaying Platforms
    --Currently only in TestBossWorld
    
    --Platforms will last for a total of 3 seconds standing
    
    --Platforms respawn after 5 seconds

--- Scripts/Misc/Platform.cs
+++ Scripts/Misc/Platform.cs
@@ -1,53 +1,75 @@
 using Godot;
 using System;
+using System.Collections.Generic;	//HashSet
 
 public class Platform : StaticBody2D
 {
+	private HashSet<object> decayObjects;
+	
+	private float decayDurability;
+	private Area2D decayArea;
+	private CollisionShape2D hitbox;
+	private Sprite sprite;
+	
+	private Timer respawnTimer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		decayObjects = new HashSet<object>();
 		
+		decayDurability = 3.0f;	//Standing on a platform for a total of 5 seconds will cause it to break
+		decayArea = GetNodeOrNull<Area2D>("DecayArea");
+		hitbox = GetNodeOrNull<CollisionShape2D>("Hitbox");
+		sprite = GetNodeOrNull<Sprite>("Sprite");
+		
+		respawnTimer = GetNodeOrNull<Timer>("RespawnTimer");
+		
+		CallDeferred("set_process", false);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		
+		//Should only be processed if >= 1 object is standing on the platform
+		decayDurability -= delta;
+		if(decayDurability <= 0.0f)
+		{
+			//Platform breaks
+			decayArea?.SetDeferred("monitoring", false);
+			hitbox?.SetDeferred("disabled", true);
+			sprite?.SetDeferred("visible", false);
+			
+			CallDeferred("set_process", false);
+			
+			respawnTimer.Start();
+		}
 	}
 	
 	private void _on_RespawnTimer_timeout()
 	{
-		Area2D decayArea = GetNodeOrNull<Area2D>("DecayArea");
+		decayDurability = 3.0f;
+		
 		decayArea?.SetDeferred("monitoring", true);
-		CollisionShape2D hitbox = GetNodeOrNull<CollisionShape2D>("Hitbox");
 		hitbox?.SetDeferred("disabled", false);
-		GetNode<Sprite>("Sprite").Visible = true;
+		sprite?.SetDeferred("visible", true);
 		
 		GD.Print("Platform respawned!!");
 	}
-
-
-	private void _on_Area2D_body_entered(object body)
-	{
-		GD.Print("Platform is now decaying!!!!!!!!!!!!!!!!!!!!!");
-		Area2D decayArea = GetNodeOrNull<Area2D>("DecayArea");
-		decayArea?.SetDeferred("monitoring", false);
-		Timer decayTimer = GetNodeOrNull<Timer>("DecayTimer");
-		decayTimer?.Start();
-	}
-
-
-	private void _on_DecayTimer_timeout()
-	{
-		Area2D decayArea = GetNodeOrNull<Area2D>("DecayArea");
-		decayArea?.SetDeferred("monitoring", false);
-		CollisionShape2D hitbox = GetNodeOrNull<CollisionShape2D>("Hitbox");
-		hitbox?.SetDeferred("disabled", true);
-		GetNode<Sprite>("Sprite").Visible = false;
-		
-		Timer respawnTimer = GetNodeOrNull<Timer>("RespawnTimer");
-		respawnTimer.Start();
-	}
-
 	
+	private void _on_DecayArea_body_entered(object body)
+	{
+		decayObjects.Add(body);
+		CallDeferred("set_process", true);
+		GD.Print("Platform is now decaying!!!!!!!!!!!!!!!!!!!!!");
+	}
+
+	private void _on_DecayArea_body_exited(object body)
+	{
+		decayObjects.Remove(body);
+		if(decayObjects.Count == 0)
+		{
+			CallDeferred("set_process", false);
+		}
+	}
 }
